c4a07e76fb865f17cb049ace8a649c8a47e3b026
whydejavu <zhqk1234567@126.com>
2019-05-10 16:27:41 +0800

    初始化springboot环境

---
+++ program/springboot/springboot-hive/src/main/java/com/whydejavu/hive/springboothive/SpringbootHiveApplication.java
@@ -0,0 +1,13 @@
+package com.whydejavu.hive.springboothive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHiveApplication.class, args);
+	}
+
+}
---
+++ program/springboot/springboot-hive/src/test/java/com/whydejavu/hive/springboothive/SpringbootHiveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.whydejavu.hive.springboothive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootHiveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
